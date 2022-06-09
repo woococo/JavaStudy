d9bda2437f399d206bb7dff6a450d492ab15e71b
woococo <j2922y@gmail.com>
2022-06-09 10:51:06 +0900

    string touppercase

--- study/Main.java
+++ study/Main.java
@@ -1,17 +1,15 @@
 package study;
 
-import java.awt.Rectangle;
-
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Rectangle box = new Rectangle(5, 10, 60, 90);
-		Rectangle box2 = box;
-		box.translate(100, 100);
-		System.out.println(box.getX());
-		System.out.println(box2.getX());
+		String greeting = "Hello, World";
+		String greeting2 = greeting;
+		greeting2.toUpperCase();
+		System.out.println(greeting);
+		System.out.println(greeting2);
 	}
 
 }
